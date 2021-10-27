f77a97566f00f11af42f81c6475df2999120019f
aditya635 <55912563+aditya635@users.noreply.github.com>
2021-10-27 23:43:12 +0530

    learning sockets

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 '''sockets implementation'''
-import os
+import socket # for socket
+import sys
+ 
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)# AFNET MEANS ipv4 and SOCK_STREAM means tcp based connection
+    print ("Socket successfully created")
+except socket.error as err:
+    print ("socket creation failed with error %s" %(err))
+ 
+# default port for socket
+port = 80
+ 
+try:
+    host_ip = socket.gethostbyname('www.google.com')
+except socket.gaierror:
+ 
+    # this means could not resolve the host
+    print ("there was an error resolving the host")
+    sys.exit()
+ 
+# connecting to the server
+s.connect((host_ip, port))
+ 
+print ("the socket has successfully connected to google")
