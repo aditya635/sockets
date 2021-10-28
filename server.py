b50139a856e1995dfce494ee58bda5414576ae4e
aditya635 <55912563+aditya635@users.noreply.github.com>
2021-10-28 23:49:27 +0530

    fuf

--- server.py
+++ server.py
@@ -25,14 +25,14 @@ print ("socket is listening")
 while True:
  
 # Establish connection with client.
-  c, addr = s.accept()    
-  print ('Got connection from', addr )
- 
-  # send a thank you message to the client. encoding to send byte type.
-  c.send('Thank you for connecting'.encode())
- 
-  # Close the connection with the client
-  c.close()
-   
-  # Breaking once connection closed
-  break
+    c, addr = s.accept()    
+    print ('Got connection from', addr )
+    
+    # send a thank you message to the client. encoding to send byte type.
+    c.send('Thank you for connecting'.encode())
+    
+    # Close the connection with the client
+    c.close()
+    
+    # Breaking once connection closed
+    break
